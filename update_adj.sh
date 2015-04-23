eef3fce3dd9b911f19454dbf76db46946ec34a34
kcruz <kaysabelle@gmail.com>
2015-04-23 15:02:24 -0700

    Update adjective fire danger rating

--- update_adj.sh
+++ update_adj.sh
@@ -2,6 +2,11 @@ echo getting new data...
 python triplescraper.py
 mv -f *.xml xml
 
+echo logging timestamps...
+git log -1 --format=%cd -- xml/lapanza.xml > timestamps
+git log -1 --format=%cd -- xml/lastablas.xml >> timestamps
+git log -1 --format=%cd -- xml/arroyogrande.xml >> timestamps
+
 echo committing...
 git add -A
 git commit -m "Update adjective fire danger rating"
